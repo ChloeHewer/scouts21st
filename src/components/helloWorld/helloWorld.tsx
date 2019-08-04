85ca1c3032516751921be02f8c2f4597f82f377e
Rhys Bray <rhysabray@hotmail.com>
2019-08-04 17:56:10 +0100

    correction

--- src/components/helloWorld/helloWorld.tsx
+++ src/components/helloWorld/helloWorld.tsx
@@ -1,52 +1,39 @@
 import * as React from "react";
 import { firestore } from "../../firebase";
 
-export interface IProps { }
+export interface IProps {}
 
-export interface IState { }
+export interface IState {}
 
 class HelloWorld extends React.Component<IProps, IState> {
   // state = { :  }
 
-
-
   render() {
-
     firestore
       .collection("argh")
       .doc()
       .set({ ugh: "hello" })
-      .then(
-        function () {
-          console.log("success")
-        }
-      )
-      .catch(
-        function () {
-          console.log("doesnt work")
-        }
-      )
+      .then(function() {
+        console.log("success");
+      })
+      .catch(function() {
+        console.log("doesnt work");
+      });
 
     firestore
       .collection("argh")
       .get()
       .then(querySnapshot => {
-        console.log(querySnapshot.docs.map(
-          doc => {
-            return { ...doc.data() }
-          }
-        ));
+        console.log(
+          querySnapshot.docs.map(doc => {
+            return { ...doc.data() };
+          })
+        );
       })
-      .catch(
-        function () {
-          console.log("doesnt work")
-        }
-      )
-    return (
-      <div>
-        Fuck you
-      </div >
-    );
+      .catch(function() {
+        console.log("doesnt work");
+      });
+    return <h1>Hello World</h1>;
   }
 }
 
