55ced6f33ee25459af1c4e2743e79376296241e3
Rhys Bray <rhysabray@hotmail.com>
2019-08-08 17:18:14 +0100

    quick header design

---
+++ src/components/header/header.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import styles from "./header.module.scss";
+import scoutsWhiteLogo from "../../assets/logos/scoutsWhite.png";
+
+export interface IProps {}
+
+export interface IState {}
+
+class Header extends React.Component<IProps, IState> {
+  render() {
+    return (
+      <header className={styles.header}>
+        <section>
+          <img src={scoutsWhiteLogo} alt="" className={styles.logo} />
+          <h1>21st Bristol Scouts Group</h1>
+        </section>
+        <ul>
+          <li>Home</li>
+          <li>Scouts</li>
+          <li>Cubs</li>
+          <li>Beavers</li>
+          <li>Volunteer</li>
+          <li>Contact</li>
+        </ul>
+      </header>
+    );
+  }
+}
+
+export default Header;
